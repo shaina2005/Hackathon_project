bc5c889d38332548a1a6daf9ddf0cfe02b01b1e1
shaina2005 <0506shyna@gmail.com>
2025-08-02 11:14:22 +0530

    dash

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Navbar from './Components/Navbar';
-import CreateTicket from './Pages/createticket';
-
-import Dashboard from './Pages/Dashboard'
-import { BrowserRouter , Routes , Route } from 'react-router-dom'
-import './App.css'
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./Components/Navbar";
+import Login from "./Pages/Login";
+import CreateTicket from "./Pages/createticket";
+import Dashboard from "./Pages/Dashboard";
+import "./App.css";
 
 function App() {
   return (
@@ -14,16 +13,11 @@ function App() {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
-<<<<<<< HEAD
-        <Route path="/create-ticket" element={<CreateTicket/>} />
-=======
-        {/* <Route path="/createticket" element={<CreateTicket />} />
-        <Route path="/ticketdetails" element={<TicketDetails />} /> */}
->>>>>>> 3f6978c420b83dcf2d5d6915b8f10ee3fa52236a
+        <Route path="/create-ticket" element={<CreateTicket />} />
+       
       </Routes>
     </BrowserRouter>
   );
-
 }
 
 export default App;
