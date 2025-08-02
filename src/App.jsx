6e72e7d4eafc7f0f7ec90a0b8dcfa7dcfa61a27a
suhani001-sudo <suhanipcte@gmail.com>
2025-08-02 09:40:40 +0530

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react'
 import Login from './Pages/Login'
+import Navbar from './Components/Navbar';
+
 import Dashboard from './Pages/Dashboard'
 import { BrowserRouter , Routes , Route } from 'react-router-dom'
 import './App.css'
@@ -8,6 +10,7 @@ function App() {
   
   return (
     <BrowserRouter>
+      <Navbar /> 
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark px-4">
+      <a className="navbar-brand text-white" href="#">MyProject</a>
+
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+
+      <div className="collapse navbar-collapse justify-content-end" id="navbarNav">
+        <ul className="navbar-nav">
+          <li className="nav-item">
+            <a className="nav-link text-white" href="#">Home</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white" href="#">Features</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white" href="#">About</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link text-white" href="#">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
