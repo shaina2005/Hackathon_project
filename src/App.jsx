a8a7c4c5e5601d5b546d37f11c0a351c1dac4c3e
suhani001-sudo <suhanipcte@gmail.com>
2025-08-02 10:48:53 +0530

    changes in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,24 @@
-import { useState } from 'react'
-import Login from './Pages/Login'
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
-
-import Dashboard from './Pages/Dashboard'
-import { BrowserRouter , Routes , Route } from 'react-router-dom'
-import './App.css'
+import Login from './Pages/Login';
+import Dashboard from './Pages/Dashboard';
+// import CreateTicket from './Pages/CreateTicket';
+// import TicketDetails from './Pages/TicketDetails';
+import './App.css';
 
 function App() {
-  
   return (
     <BrowserRouter>
-      <Navbar /> 
+      <Navbar />
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        {/* <Route path="/createticket" element={<CreateTicket />} />
+        <Route path="/ticketdetails" element={<TicketDetails />} /> */}
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
