be5d84af42a3780400f15d789cdc85f9ae19ca20
shaina2005 <0506shyna@gmail.com>
2025-08-02 09:27:48 +0530

    Dashboard fixed

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
-import './index.css'
-import App from './App.jsx'
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
