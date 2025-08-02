e03cbeb6a773ea742922e1a1ea132726089e241e
princekhatri88 <princekhatriludhiana@gmail.com>
2025-08-02 11:52:35 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Navbar from './Components/Navbar';
 import Login from './Pages/Login';
 import Dashboard from './Pages/Dashboard';
-import CreateTicket from './Pages/CreateTicket';
-import TicketDetails from './Pages/TicketDetails';
+import CreateTicket from './Pages/createticket'; 
 import './App.css';
 
 function App() {
@@ -12,13 +11,10 @@ function App() {
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/login" element={<Login/>} />
+        <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
-
-        <Route path="/create-ticket" element={<CreateTicket/>} />
-        <Route path="/ticket-details" element={<TicketDetails />} />
         <Route path="/createticket" element={<CreateTicket />} />
-        <Route path="/ticketdetails" element={<TicketDetails />} />
+
       </Routes>
     </BrowserRouter>
   );
